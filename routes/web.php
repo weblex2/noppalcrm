93d85458eb36c1f27411ead46b098ff8ece844b6
Alex Noppenberger <noppenbe@gmx.de>
2025-06-15 17:05:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::redirect('/', '/admin', 301);
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
