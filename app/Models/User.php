5cdc83fe905851fd984ad94a7b30d02b009954e4
Alex Noppenberger <noppenbe@gmx.de>
2025-06-15 20:05:17 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    }
 }
