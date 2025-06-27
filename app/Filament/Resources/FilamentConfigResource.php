c18fcab7836ef4d6d48e0d33156f3cd9f1122803
Alex <noppenbe@gmx.de>
2025-06-27 16:41:13 +0200

    Update FilamentConfigResource.php

--- app/Filament/Resources/FilamentConfigResource.php
+++ app/Filament/Resources/FilamentConfigResource.php
@@ -19,6 +19,8 @@ class FilamentConfigResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Configuration';
+
     public static function form(Form $form): Form
     {
         return $form
