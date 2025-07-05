b059a6679b347ab34e4a3b798a30283e3c118a99
Alex Noppenberger <noppenbe@gmx.de>
2025-07-05 22:10:07 +0200

    cr

---
+++ app/Traits/CustomerRelations.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
+trait CustomerRelations
+{
+
+    ##
+}
