c0842c4080cf801ebc43055ee63a21495292ae5e
Alex Noppenberger <noppenbe@gmx.de>
2025-07-05 19:34:51 +0200

    Update setup.blade.php

--- resources/views/filament/pages/setup.blade.php
+++ resources/views/filament/pages/setup.blade.php
@@ -1,6 +1,10 @@
 <x-filament::page>
-    <h2 class="mb-4 text-xl font-bold">Neue Ressource erstellen</h2>
+    <h2 class="mb-4 text-xl font-bold">Ceate new Ressource</h2>
     @livewire('resource-creator')
-    <h2 class="mb-4 text-xl font-bold">Ressource bearbeiten</h2>
+
+    <h2 class="mb-4 text-xl font-bold">Ceate new Relaton</h2>
+    @livewire('relation-manager-creator')
+
+    <h2 class="mb-4 text-xl font-bold">Edit Ressource</h2>
     @livewire('filament-resources-list')
 </x-filament::page>
