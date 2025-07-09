171c56014ee8b0682273e987c2b7d3780cde55d9
Alex <noppenbe@gmx.de>
2025-07-09 14:04:10 +0200

    Update FilamentConfigResource.php

--- app/Filament/Resources/FilamentConfigResource.php
+++ app/Filament/Resources/FilamentConfigResource.php
@@ -91,6 +91,7 @@ class FilamentConfigResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
