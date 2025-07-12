3b0456183806fa4346949d47c2a2f0e2253414ca
Alex Noppenberger <noppenbe@gmx.de>
2025-07-12 16:14:32 +0200

    Update CreateFilamentConfig.php

--- app/Filament/Resources/FilamentConfigResource/Pages/CreateFilamentConfig.php
+++ app/Filament/Resources/FilamentConfigResource/Pages/CreateFilamentConfig.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateFilamentConfig extends CreateRecord
 {
     protected static string $resource = FilamentConfigResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return FilamentConfigResource::getUrl('index');
+    }
 }
