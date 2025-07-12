f1cd7fbbd4072549360ad13a8097ba47faafcdaa
Alex Noppenberger <noppenbe@gmx.de>
2025-07-12 16:18:38 +0200

    Update FilamentConfig.php

--- app/Models/FilamentConfig.php
+++ app/Models/FilamentConfig.php
@@ -1,5 +1,5 @@
 <?php
-
+## DON'T TOUCH!
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class FilamentConfig extends Model
 {
     protected $guarded = ['id'];
+
+    public static function getFiltersFor(string $resource, string $field): array
+    {
+        return self::query()
+            ->where('type', 'option')
+            ->where('field', $field)
+            ->where('resource', $resource)
+            ->pluck('value', 'key') // ergibt ein Array: ['key1' => 'value1', ...]
+            ->toArray();
+    }
 }
