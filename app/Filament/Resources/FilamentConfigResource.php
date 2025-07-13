913f03218cde50a17be0dd41cc49ff0e84886daa
Alex Noppenberger <noppenbe@gmx.de>
2025-07-13 22:36:51 +0200

    Update FilamentConfigResource.php

--- app/Filament/Resources/FilamentConfigResource.php
+++ app/Filament/Resources/FilamentConfigResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
